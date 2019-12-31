cbd2bec51991ff57ed403d5bab3a8ec69d0f16e7
CorndogMD <47642061+officialCorndog@users.noreply.github.com>
2019-12-30 21:24:14 -0500

    dot test block function

--- POE BOT TEST TYPING.ps1
+++ POE BOT TEST TYPING.ps1
@@ -10,42 +10,28 @@ Thx yall
 Write-Host "POE Chat Bot Destroyer"
 Write-Host "https://github.com/nikgame33/POE-ChatBotDestroyer"
 Write-Host "Initializing Log Monitor"
-
-#init
-
-#check if the path is already set and if not it will add thhe path to the .txt
-$POE_ChatLog = "$PSScriptRoot\POE_LOG_PATH.txt"
-if("" -like (Get-Content $POE_ChatLog)){
-    $gamepath = (Get-Process PathOf*).path
-    $gamepath = $gamepath -replace '(?:.(?!\\))+$','\logs\client.txt' 
-    $gamepath |Out-File -FilePath $POE_ChatLog
-}
-
-
-#spambot text check
-$regexBOT = ".*([Cc]heap|[Fa]ast|[Ss]afe).*"
-#index to keep the shell clean
-$shellindex = 0
-
 Write-Host "Complete"
 Write-Host "Monitoring Chat Logs ACTIVE"
 Get-Date
 Write-Host "============================"
 
+#Set POE Chat Log Location Here
+$POE_ChatLog = "C:\Program Files (x86)\Grinding Gear Games\Path of Exile\logs\Client.txt"
+
+#spambot text check
+$regexBOT = ".*([Cc]heap|[Fa]ast|[Ss]afe).*"
 
 #TODO - add additional spam detection (currency store post, etc)
+
 
 #when spam bot text is detected, call AHK script
 while($true){
 
-    Get-Content -Tail 1 -Wait -Path (get-content $POE_ChatLog) | ForEach-Object{
+    Get-Content -Tail 1 -Wait -Path $POE_ChatLog | ForEach-Object{
         if($_ -match $regexBOT){
-            $shellindex++
-            $timestamp ="{0:hh}:{0:mm}" -f (Get-Date) 
-            write-host "Detected at $timestamp" 
-            if($shellindex -gt 9){
-                Clear-Host
-            }
+            write-host "Detected" 
+            Get-Date
+            
             #SET AS TEST SCRIPT, *CHANGE BEFORE RELEASE*
             . "$PSScriptRoot\BLOCKTEST.ahk"
         }
